940c17173373a6a8293ecd9445aa965984c581a6
Phiphou <phiphou@phiphou.com>
2017-03-23 21:08:08 +0100

    First commit
    First commit
    
    Update description
    First commit
    
    
    First commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # mbtiles2ungzpbf
+
+Very simple project to convert an mbtiles file into a set of ungzipped pbf tiles for local serving.
+
+## install
+
+With [npm](https://npmjs.org) do:
+
+```
+npm install mbtiles2ungzpbf -g
+```
+
+## example
+
+```
+$ mbtiles2ungzpbf -f tiles.mbtiles
+```
+
+## license
+
+MIT
---
+++ bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+var Buffer = require('buffer').Buffer
+var zlib = require('zlib')
+var sqlite3 = require('sqlite3')
+var writeFile = require('writefile')
+
+var argv = require('yargs')
+    .usage('Usage: <command> [options]')
+    .command('count', 'Count the lines in a file')
+    .example('count -f tiles.mbtiles', 'the mbtiles file.')
+    .alias('f', 'file')
+    .describe('f', 'the mbtiles file.')
+    .demandOption(['f'])
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2017')
+    .argv;
+
+var db = new sqlite3.Database(argv.file, function (err) {});
+
+db.each("SELECT * FROM tiles", function (err, row) {
+  var raw = zlib.gunzipSync(new Buffer(row.tile_data))
+  var y = (1 << row.zoom_level) - row.tile_row - 1
+  writeFile(`tiles/${row.zoom_level}/${row.tile_column}/${y}.pbf`, raw)
+})
